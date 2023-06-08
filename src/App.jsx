e7cd4a1c3ff7512afccce581933bb6dcbbd39a58
Ayo Sam <ayotolusam@gmail.com>
2023-06-08 19:24:28 +0100

    Initial setup commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hello, I made skeletal, unstyled and reusable react components available for your use.</h1>
+    </div>
+  )
+}
+
+export default App
